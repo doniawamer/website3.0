5b685c4ecab516322a7041f12cb25f539688381e
Donia Amer <doniawamer@gmail.com>
2021-03-18 07:13:37 -0700

    Home/About/Contact OK (#3)
    * Home/About/Contact OK
    
    * buuild files?
    
    * rebuilt
    
    Co-authored-by: damer <damer@deloitte.ca>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import Header from "./components/header/header.jsx";
 import Home from "./components/home/home.jsx";
 import About from "./components/about/about.jsx";
+import Contact from "./components/contact/contact.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main id="main" className="app-container">
       <Home />
       <About />
+      <Contact />
       </main>
     </div>
   );
--- src/assets/fonts/copy/copy.js
+++ src/assets/fonts/copy/copy.js
@@ -1,4 +1,8 @@
 let copy = {
+    "Header": {
+        "Skip": "Skip to main content",
+        "Nav": ["Home", "About", "Projects", "Contact"]
+    },
     "Home": {
         "PreTitle": "Hi there, my name is",
         "Title": "Donia Amer",
@@ -8,7 +12,7 @@ let copy = {
     "About": {
         "Title": "About me",
         "Body": [
-            "Hey there! I’m Donia, a computer engineer based in Toronto, Canada. Fueled by the challenge of solving complex problems and lots of iced coffee, I’m always inspired to build products that start with the user and ends with innovation.",
+            "Hey there! I’m Donia, a computer engineer based in Toronto, Canada. Fueled by the challenge of solving complex problems and lots of iced coffee, I’m always inspired to build products that start with the user and end with innovation.",
             "After graduating from York University and interning at IBM, I joined the front-end engineering team at Deloitte Digital, where I work on impactful projects and make ideas come to life!",
             "My current toolset includes React, Angular, Typescript, and other various frameworks and libraries related to them. "
         ]
@@ -16,7 +20,11 @@ let copy = {
     "Portfolio": {},
     "Contact": {
         "Title": "Find me in my  corner of the internet",
-        "Subtitle": `or check out my <a href="/pdfs/resume.pdf" className="contact__resume" download>resume</a>!`,
+        "Subtitle": [
+            "or check out my", 
+            "resume",
+            "!"
+        ],
     }
 }
 
